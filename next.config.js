e93afd1557997711b4c2d58e355f199d96135bc0
SzymonAdamski <szymon.adamski@microsoft.wsei.edu.pl>
2025-12-04 13:59:33 +0100

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/nextjs-tictactoe-lab6-10',
+  assetPrefix: '/nextjs-tictactoe-lab6-10/',
 }
 
 module.exports = nextConfig
