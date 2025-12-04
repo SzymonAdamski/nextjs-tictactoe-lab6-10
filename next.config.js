66d3e0f66f2024e1b10b23872200b7cd6dda5e84
SzymonAdamski <szymon.adamski@microsoft.wsei.edu.pl>
2025-12-04 16:25:34 +0100

    Fix Firebase Hosting: remove basePath and update rewrites

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/nextjs-tictactoe-lab6-10',
-  assetPrefix: '/nextjs-tictactoe-lab6-10/',
 }
 
 module.exports = nextConfig
